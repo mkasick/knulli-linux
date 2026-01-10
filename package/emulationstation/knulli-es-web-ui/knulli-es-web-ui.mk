01df85df627eddbb6df6923c9c796ce48df3efab
Mike Kasick <mike@kasick.org>
2026-01-10 16:49:41 -0500

    knulli-es-web-ui: Create /usr/share/knulli/services in target.

--- package/emulationstation/knulli-es-web-ui/knulli-es-web-ui.mk
+++ package/emulationstation/knulli-es-web-ui/knulli-es-web-ui.mk
@@ -10,6 +10,7 @@ KNULLI_ES_WEB_UI_SOURCE =
 
 define KNULLI_ES_WEB_UI_RESOURCES
 	$(INSTALL) -m 0755 -d $(TARGET_DIR)/usr/share/emulationstation/resources/services
+	$(INSTALL) -m 0755 -d $(TARGET_DIR)/usr/share/knulli/services
 	$(INSTALL) -m 0755 -d $(TARGET_DIR)/usr/bin
     $(INSTALL) -m 0755 -d $(TARGET_DIR)/usr/share/knulli/services
 	$(INSTALL) -m 0644 -D $(BR2_EXTERNAL_KNULLI_PATH)/package/emulationstation/knulli-es-web-ui/web/* \
